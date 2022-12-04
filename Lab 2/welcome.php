7f31e7cf8b298537295eb656ba489f12c7576567
MavelSterling <mavelynsl@gmail.com>
2022-12-04 17:13:37 -0500

    Se crea el archivo php

---
+++ Lab 2/welcome.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+<?php
+echo "Bienvenido, " . $_POST["name"] . "<br>";
+echo "Su username es , " . $_POST["user_name"] . "<br>";
+echo "Y su contraseña es " . $_POST["password"] . "<br>";
+?>
+</body>
+</html
