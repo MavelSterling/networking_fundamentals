5761d3d28c38da3391503b4a748c39f85ae972e2
MavelSterling <mavelynsl@gmail.com>
2022-12-04 17:26:58 -0500

    Se crea el formulario con metodo get

--- Lab 2/welcome.php
+++ Lab 2/welcome.php
@@ -2,9 +2,18 @@
 <body>
 
 <?php
-echo "Bienvenido, " . $_POST["name"] . "<br>";
+echo "Bienvenid@, " . $_POST["name"] . "<br>";
 echo "Su username es , " . $_POST["user_name"] . "<br>";
 echo "Y su contraseña es " . $_POST["password"] . "<br>";
 ?>
 </body>
+
+<body>
+<?php
+echo "Bienvenid@, " . $_GET["name"] . "<br>";
+echo "Su username es , " . $_GET["user_name"] . "<br>";
+echo "Y su contraseña es " . $_GET["password"] . "<br>";
+?>
+</body>
+
 </html
