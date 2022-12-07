9a7e9c9992cf63e7a4d724ed3006a324455ce2b6
MavelSterling <mavelynsl@gmail.com>
2022-12-06 22:57:37 -0500

    Update welcome.php

--- Lab 2/welcome.php
+++ Lab 2/welcome.php
@@ -1,19 +1,6 @@
-<html>
-<body>
-
 <?php
-echo "Bienvenid@, " . $_POST["name"] . "<br>";
-echo "Su username es , " . $_POST["user_name"] . "<br>";
-echo "Y su contraseña es " . $_POST["password"] . "<br>";
-?>
-</body>
+    echo "Bienvenid@, " . $_REQUEST["name"] . "<br>";
+    echo "Su username es , " . $_REQUEST["user_name"] . "<br>";
+    echo "Y su contraseña es " . $_REQUEST["password"] . "<br>";
 
-<body>
-<?php
-echo "Bienvenid@, " . $_GET["name"] . "<br>";
-echo "Su username es , " . $_GET["user_name"] . "<br>";
-echo "Y su contraseña es " . $_GET["password"] . "<br>";
 ?>
-</body>
-
-</html
